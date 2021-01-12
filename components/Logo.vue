d6ab538639477ba29752ba1855e64a2ed7694d91
Michael Dyczkowski <michaeldyczkowski@gmail.com>
2021-01-12 21:51:47 +0000

    added header

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="~assets/images/therapy-space-logo.svg" alt="therapy-space-logo">
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ components/navigation/TheHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header-wrapper">
+    <b-navbar toggleable="sm">
+      <b-navbar-toggle target="nav-text-collapse" />
+
+      <b-navbar-brand href="/">Therapy Space</b-navbar-brand>
+
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/">
+            Welcome
+          </b-nav-item>
+          <b-nav-item-dropdown text="Services">
+            <b-dropdown-item href="/consuelling">
+              Consuelling
+            </b-dropdown-item>
+            <b-dropdown-item href="/cbt">
+              CBT
+            </b-dropdown-item>
+            <b-dropdown-item href="/emdr">
+              EMDR
+            </b-dropdown-item>
+            <b-dropdown-item href="/walk_and_talk">
+              Walk & Talk
+            </b-dropdown-item>
+            <b-dropdown-item href="/supervision">
+              Supervision
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item href="/what_to_expect">
+            What to Expect
+          </b-nav-item>
+          <b-nav-item href="/contact">
+            Contact
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
