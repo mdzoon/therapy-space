eb386600cb56a4070e8810751db627ff9c469756
mdzoon <michaeldyczkowski@gmail.com>
2022-05-08 14:53:32 +0100

    improved footer

--- components/navigation/TheFooter.vue
+++ components/navigation/TheFooter.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="footer-wrapper">
-    <hr>
-    <div class="container">
-      <div class="row py-4 footer-inner">
-        <div class="col-md-6">
-          <nav class="nav flex-row text-left">
-            <a class="nav-link" href="/">Home</a>
-            <a class="nav-link" href="/privacy-policy">Privacy Policy</a>
-          </nav>
+    <div class="page-break"></div>
+    <b-container>
+      <b-row class="my-5">
+        <b-col md="6">
+          <nav class="nav flex-column">
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/privacy-policy">Privacy Policy</NuxtLink>
+            <NuxtLink to="/cookies-policy">Cookies Policy</NuxtLink>
+          </nav>          
+        </b-col>
+        <div class="col-md-6 text-right logo-wrapper">
+          <a href="https://www.bacp.co.uk" target="_blank"><img src="~/assets/images/BACP-logo.svg" alt="British Association for Counselling and Psychotherapy Logo"></a>          
+          <a href="https://babcp.com" target="_blank"><img src="~/assets/images/BABCP-logo.svg" alt="British Association for Behavioural and Cognitive Psychotherapies Logo"></a>
         </div>
-        <div class="col-md-6 text-right">
-          <div>
-            Copyright &copy; {{ currentYear }} Therapy Space
-          </div>
-          <div>
-            Created by <a href="https://www.michaeldyczkowski.co.uk" target="_blank">Michael Dyczkowski</a>
-          </div>
-        </div>
-      </div>
-    </div>
+      </b-row>
+      <b-row class="my-5">
+        <b-col cols="12" class="text-center footer-advice">
+          <p>If you are feeling unsafe, or need immediate support, you are advised to contact:</p>
+          <p>Your GP surgery, Out of Hours GP: 111, in case of emergency your local A&E</p>
+          <p>Mental Health Matters  helpline: 08001070160</p>
+          <p>Samaritans helpline : 08457909090 </p>
+        </b-col>
+      </b-row>
+      <b-row class="my-5 copyright-wrapper">
+        <b-col md="6">
+          Copyright &copy; {{ currentYear }} Therapy Space
+        </b-col>
+        <b-col md="6">
+          Created by <a href="https://www.michaeldyczkowski.co.uk" target="_blank">Michael Dyczkowski</a>
+        </b-col>
+      </b-row>      
+    </b-container>
   </div>
 </template>
 
@@ -31,3 +44,52 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.footer-wrapper a {
+  color: var(--colour-font);
+}
+.footer-wrapper a:hover {
+  color: var(--colour-link-hover);
+  text-decoration: none;
+}
+.page-break {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  height: 0.25rem;
+  background-image: url('~/assets/images/page-break.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0.5;
+}
+.logo-wrapper, .copyright-wrapper {
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+}
+.logo-wrapper img {
+  height: 5rem;
+}
+.footer-advice {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+.footer-advice p {
+  font-size: smaller;
+  margin-bottom: 0.15rem;
+}
+.copyright-wrapper > div {
+  text-align: center;
+}
+@media only screen and (max-width: 767px) {
+  nav.nav.flex-column {
+    align-items: center;
+    padding-bottom: 4rem;
+  }
+}
+@media only screen and (min-width: 768px) {
+  .footer-wrapper > div.container {
+    padding-top: 4rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,23 @@
 <template>
-  <b-container fluid>
-    <b-container>
-      <b-row>
-        <b-col lg="6" class="left">
-          <div class="image-box">
-            <img src="@/assets/images/grazyna-portrait.jpg" alt="default-portrait" class="portrait-img">
-          </div>
-        </b-col>
-        <b-col lg="6" class="right">
-          <div class="info-box">
-            <p>I am BACP Accredited Integrative Counsellor and BABCP Accredited Cognitive Behavioural Psychotherapist and Supervisor. I am also EMDR practitioner and UK Accredited Mediator.</p>
-            <p>I have been working for over 10 years with clients in the NHS and in private setting. My training in different therapeutic approaches combined with my clinical experience allows me to offer in-depth assessment of your issues and provide treatment which is most suitable for your particular needs.</p>
-            <p>I see my role as helping you to understand yourself and your interactions with others clearer. This will help you to make better-informed and more beneficial choices in your life. I believe that therapeutic relationship plays a key role in positive therapy outcomes.</p>
-            <p>I have worked effectively with people who present with a range of issues including depression, social anxiety, low self-esteem, general anxiety, panic attacks, OCD, PTSD and childhood trauma. I provide support for those who have experienced loss or bereavement in their lives. If you have any family problems that impact on your life you might want to address this in therapy. I also offer help with anger management.</p>
-            <p>Since Covid I have been offering therapy using Zoom or WhatsApp. I send secure Zoom link to the session by e-mail and that can be easily accessed on the laptop or smartphone.</p>
-            <p>I offer sessions in English and in Polish.</p>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
-  </b-container>
+  <main class="container">
+    <b-row>
+      <b-col lg="6" class="left">
+        <div class="image-box">
+          <img src="@/assets/images/grazyna-portrait.jpg" alt="default-portrait" class="portrait-img">
+        </div>
+      </b-col>
+      <b-col lg="6" class="right">
+        <div class="info-box">
+          <p>I am BACP Accredited Integrative Counsellor and BABCP Accredited Cognitive Behavioural Psychotherapist and Supervisor. I am also EMDR practitioner and UK Accredited Mediator.</p>
+          <p>I have been working for over 10 years with clients in the NHS and in private setting. My training in different therapeutic approaches combined with my clinical experience allows me to offer in-depth assessment of your issues and provide treatment which is most suitable for your particular needs.</p>
+          <p>I see my role as helping you to understand yourself and your interactions with others clearer. This will help you to make better-informed and more beneficial choices in your life. I believe that therapeutic relationship plays a key role in positive therapy outcomes.</p>
+          <p>I have worked effectively with people who present with a range of issues including depression, social anxiety, low self-esteem, general anxiety, panic attacks, OCD, PTSD and childhood trauma. I provide support for those who have experienced loss or bereavement in their lives. If you have any family problems that impact on your life you might want to address this in therapy. I also offer help with anger management.</p>
+          <p>Since Covid I have been offering therapy using Zoom or WhatsApp. I send secure Zoom link to the session by e-mail and that can be easily accessed on the laptop or smartphone.</p>
+          <p>I offer sessions in English and in Polish.</p>
+        </div>
+      </b-col>
+    </b-row>
+  </main>
 </template>
 
 <script>
@@ -27,15 +25,6 @@ export default {}
 </script>
 
 <style scoped>
-.container-fluid {
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  background-color: var(--veryLight-grayscale);
-  /* background-image: url("~assets/images/therapy-space-logo.svg");
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover; */
-}
 .left,
 .right {
   display: flex;
