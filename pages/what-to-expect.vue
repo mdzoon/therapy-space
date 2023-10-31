71f016255c0f0bb6890983dd418890528d78ba4c
mdzoon <michaeldyczkowski@gmail.com>
2023-10-31 23:48:07 +0000

    text adjustments

--- pages/what-to-expect.vue
+++ pages/what-to-expect.vue
@@ -73,8 +73,8 @@
           <b-collapse id="how-do-i-work" accordion="accordion" role="tabpanel">
             <b-card-body>
               <b-card-text>
-                <p>I respond to your query by e-mail or telephone according to your stated preference. If you wish I am happy to have 10-15 min discussion over the phone which is free of charge.</p>
-                <p>I offer an initial assessment of your psychological needs which takes up to 60 minutes and the fee is £70. The assessment gives us an opportunity to meet and discuss how we can work together. I aim to gather some information about yourself, your current problems that you wish to address in therapy and any relevant past information that have contributed to your current issues. I aim to answer any questions you have and clarify your therapy expectations. The assessment helps us both decide how we can continue working together.</p>
+                <p>I respond to your query by e-mail or telephone according to your stated preference. If you wish I am happy to have a 10-15 min discussion over the phone which is free of charge.</p>
+                <p>I offer an initial assessment of your psychological needs which takes up to 60 minutes and the fee is £70. The assessment gives us an opportunity to meet and discuss how we can work together. I aim to gather some information about yourself, your current problems that you wish to address in therapy and any relevant past information that has contributed to your current issues. I aim to answer any questions you have and clarify your therapy expectations. The assessment helps us both decide how we can continue working together.</p>
                 <p>My fee is £70 for 50 minutes of therapy session.</p>
                 <BookingButton class="d-block mt-5" link='/contact' design="boxed">Book Appointment</BookingButton>
               </b-card-text>
