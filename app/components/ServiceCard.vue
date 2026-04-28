7a8cb6971724cbd6872815cf71c355a272cb842d
mdzoon <michaeldyczkowski@gmail.com>
2026-04-28 22:03:15 +0200

    ServiceCard.vue css to scss

--- app/components/ServiceCard.vue
+++ app/components/ServiceCard.vue
@@ -34,193 +34,112 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
     margin-top: 6rem;
     margin-bottom: 6rem;
 }
 
-.service-wrapper:nth-of-type(2n+1) {
-    text-align: left;
-    border-left: 5px solid;
-    border-color: var(--colour-primary);
-    /* box-shadow: var(--box-shadow, var(--webkit-box-shadow)); */
+.service-wrapper {
     border-radius: var(--radius-small);
-    /* ff 3.6+ */
-    background:-moz-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%); 
-    /* safari 5.1+,chrome 10+ */
-    background:-webkit-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* opera 11.10+ */
-    background:-o-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* ie 6-9 */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#FFFFFF', endColorstr='#FFFFFF', GradientType=0 );
-    /* ie 10+ */
-    background:-ms-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* global 94%+ browsers support */
-    background:linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-}
-.service-wrapper:nth-of-type(2n) {
-    text-align: right;
-    border-right: 5px solid;
-    border-color: var(--colour-secondary);
-    /* box-shadow: var(--box-shadow, var(--webkit-box-shadow)); */
-    border-radius: var(--radius-small);
-    /* ff 3.6+ */
-    background:-moz-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%); 
-    /* safari 5.1+,chrome 10+ */
-    background:-webkit-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* opera 11.10+ */
-    background:-o-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* ie 6-9 */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#FFFFFF', endColorstr='#FFFFFF', GradientType=0 );
-    /* ie 10+ */
-    background:-ms-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-    /* global 94%+ browsers support */
-    background:linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
-}
-.service-wrapper h3 {
-    letter-spacing: 8px;
-    font-size: 1.75rem;
-}
-.service-wrapper:nth-of-type(2n+1) h3 {
-    text-align: left;
-}
-.service-wrapper:nth-of-type(2n) h3 {
-    text-align: right;
-}
 
-@media only screen and (max-width: 499px) {
-    .therapy-card :deep(button) {
-        width: 100%;
+    h3 {
+        letter-spacing: 8px;
+        font-size: 1.75rem;
+    }
+
+    &:nth-of-type(2n+1) {
+        text-align: left;
+        border-left: 5px solid var(--colour-primary);
+        background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
+
+        @media only screen and (min-width: 576px) {
+            background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
+        }
+
+        @media only screen and (min-width: 768px) {
+            padding-left: 2rem;
+            background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
+        }
+    }
+
+    &:nth-of-type(2n) {
+        text-align: right;
+        border-right: 5px solid var(--colour-secondary);
+        background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 75%, rgba(255, 255, 255, 0) 100%);
+
+        @media only screen and (min-width: 576px) {
+            background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
+        }
+
+        @media only screen and (min-width: 768px) {
+            padding-right: 2rem;
+            background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
+        }
     }
 }
 
 .therapy-card {
     padding-top: 5em;
     padding-bottom: 5em;
-    /* ff 3.6+ */
-    background:-moz-radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
-    /* safari 5.1+,chrome 10+ */
-    background:-webkit-radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
-    /* opera 11.10+ */
-    background:-o-radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
-    /* ie 10+ */
-    background:-ms-radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
-    /* global 92%+ browsers support */
-    background:radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
-}
+    background: radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.07) 69%, rgba(238, 130, 238, 0) 100%);
 
-.therapy-card .anchor {
-    display: block;
-    position: relative;
-    top: -150px;
-    visibility: hidden;  
-}
+    .anchor {
+        display: block;
+        position: relative;
+        top: -150px;
+        visibility: hidden;
+    }
 
-.therapy-card .therapy-body {
-    margin-top: 5em;
-    margin-bottom: 5em;
-}
-.therapy-card .therapy-body .testimonial {
-    margin-top: 3em;
-    font-size: smaller;
-}
+    .therapy-body {
+        margin-top: 5em;
+        margin-bottom: 5em;
 
-.therapy-card .therapy-body :deep(p) {
-    margin-top: 3em;
-}
-.therapy-card .therapy-body :deep(a) {
-    margin: auto;
-}
-.therapy-card .therapy-body :deep(ul) {
-    list-style-type: none;
-    text-align: justify;
-    padding: 0 10% 0;
-}
-@media only screen and (min-width: 992px) {
-    .therapy-card .therapy-body :deep(ul) {
-        text-align: inherit;
-        padding: 0;
-    } 
-}
+        .testimonial {
+            margin-top: 3em;
+            font-size: smaller;
+        }
 
-@media only screen and (max-width: 575px) {
-    .therapy-card {
+        :deep(p) {
+            margin-top: 3em;
+        }
+
+        :deep(a) {
+            margin: auto;
+        }
+
+        :deep(ul) {
+            list-style-type: none;
+            text-align: justify;
+            padding: 0 10% 0;
+
+            @media only screen and (min-width: 992px) {
+                text-align: inherit;
+                padding: 0;
+            }
+        }
+    }
+
+    @media only screen and (max-width: 575px) {
         padding-top: 2em;
         padding-bottom: 2em;
+
+        .therapy-header h3 {
+            letter-spacing: 6px;
+            font-size: 1.5rem;
+        }
+
+        .therapy-body {
+            margin-top: 2em;
+            margin-bottom: 2em;
+            font-size: 1rem;
+        }
     }
-    .therapy-card .therapy-header h3 {
-        letter-spacing: 6px;
-        font-size: 1.5rem;
+
+    @media only screen and (max-width: 499px) {
+        :deep(button) {
+            width: 100%;
+        }
     }
-    .therapy-card .therapy-body {
-        margin-top: 2em;
-        margin-bottom: 2em;
-    }
-    .therapy-card .therapy-body {
-        font-size: 1rem;
-    }  
-}
-@media only screen and (min-width: 576px) {
-    .service-wrapper:nth-of-type(2n+1) {
-        /* ff 3.6+ */
-        background:-moz-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%); 
-        /* safari 5.1+,chrome 10+ */
-        background:-webkit-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* opera 11.10+ */
-        background:-o-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* ie 6-9 */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#FFFFFF', endColorstr='#FFFFFF', GradientType=0 );
-        /* ie 10+ */
-        background:-ms-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* global 94%+ browsers support */
-        background:linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-    }
-    .service-wrapper:nth-of-type(2n) {
-        /* ff 3.6+ */
-        background:-moz-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%); 
-        /* safari 5.1+,chrome 10+ */
-        background:-webkit-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* opera 11.10+ */
-        background:-o-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* ie 6-9 */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#FFFFFF', endColorstr='#FFFFFF', GradientType=0 );
-        /* ie 10+ */
-        background:-ms-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-        /* global 94%+ browsers support */
-        background:linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
-    }
-}
-@media only screen and (min-width: 768px) {
-    .service-wrapper:nth-of-type(2n+1) {
-        padding-left: 2rem;
-        /* ff 3.6+ */
-        background:-moz-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%); 
-        /* safari 5.1+,chrome 10+ */
-        background:-webkit-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* opera 11.10+ */
-        background:-o-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* ie 6-9 */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#EE82EE', endColorstr='#FFFFFF', GradientType=0 );
-        /* ie 10+ */
-        background:-ms-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* global 94%+ browsers support */
-        background:linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);    
-    }
-    .service-wrapper:nth-of-type(2n) {
-        padding-right: 2rem;
-        /* ff 3.6+ */
-        background:-moz-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%); 
-        /* safari 5.1+,chrome 10+ */
-        background:-webkit-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* opera 11.10+ */
-        background:-o-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* ie 6-9 */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#FFFFFF', endColorstr='#EE82EE', GradientType=0 );
-        /* ie 10+ */
-        background:-ms-linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);
-        /* global 94%+ browsers support */
-        background:linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(238, 130, 238, 0) 100%);    
-    }  
 }
 </style>
